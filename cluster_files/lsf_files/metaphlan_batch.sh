48a2a52f34964ccb59c17d6470f13ad05f342dad
kevmu <muirheadkevins@gmail.com>
2021-03-16 12:56:15 -0600

    Update metaphlan_batch.sh

--- cluster_files/lsf_files/metaphlan_batch.sh
+++ cluster_files/lsf_files/metaphlan_batch.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-#BSUB -J metqc_batch_run
+#BSUB -J metaphlan_batch_run
 #BSUB -n 1
 #BSUB -R "span[hosts=1]"
 #BSUB -W 60:00
-#BSUB -o metqc_batch_run_%J.out
-#BSUB -e metqc_batch_run_%J.err
+#BSUB -o metaphlan_batch_run_%J.out
+#BSUB -e metaphlan_batch_run_%J.err
 
 log_dir="$(pwd)"
-log_file="logs/metqc.log.txt"
+log_file="logs/metaphlan-analysis.log.txt"
 num_jobs=20
 
 snakemake --cluster-config cluster.json --cluster 'bsub -n {cluster.n} -R {cluster.resources} -W {cluster.walllim} -We {cluster.time} -M {cluster.maxmem} -oo {cluster.output} -e {cluster.error}' --jobs $num_jobs --use-conda &> $log_dir/$log_file
