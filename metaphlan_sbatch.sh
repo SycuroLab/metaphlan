d015926d07b8086024e9a342b0ce9e655ee9225f
kevmu <muirheadkevins@gmail.com>
2021-07-10 12:10:54 -0600

    Update metaphlan_sbatch.sh
    Updated num_jobs to 50 as default. Also added shell commands for copying over all snakemake related files including logs so that a particular project parameter specifications, conda environment used (program versions for publications) are available and not confusing to follow. Also added the sbatch command job name so that it is easier to see in the squeue list.

--- metaphlan_sbatch.sh
+++ metaphlan_sbatch.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
+#SBATCH --job-name="metaphlan_sbatch"
 #SBATCH --partition=cpu2019
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=2-00:00:00
 #SBATCH --mem=5G
-#SBATCH --error=metaphlan_sbatch_run.%J.err
-#SBATCH --output=metaphlan_sbatch_run.%J.out
+#SBATCH --error=metaphlan_run.%J.err
+#SBATCH --output=metaphlan_run.%J.out
 
 log_dir="$(pwd)"
 log_file="logs/metaphlan-analysis.log.txt"
-num_jobs=10
+num_jobs=50
 
 echo "started at: `date`"
 
 snakemake --cluster-config cluster.json --cluster 'sbatch --partition={cluster.partition} --cpus-per-task={cluster.cpus-per-task} --nodes={cluster.nodes} --ntasks={cluster.ntasks} --time={cluster.time} --mem={cluster.mem} --output={cluster.output} --error={cluster.error}' --jobs $num_jobs --use-conda &> $log_dir/$log_file
+
+output_dir=$(grep "output_dir" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
+list_files=$(grep "list_files" < config.yaml | grep -v "#" | cut -d ' ' -f2 | sed 's/"//g')
+
+snakemake_file_dir="${output_dir}/snakemake_files"
+mkdir -p $snakemake_file_dir
+
+cp $list_files $snakemake_file_dir
+
+cp Snakefile $snakemake_file_dir
+cp config.yaml $snakemake_file_dir
+cp cluster.json $snakemake_file_dir
+cp metaphlan_sbatch.sh $snakemake_file_dir 
+
+cp -rf logs $snakemake_file_dir
+cp -rf utils $snakemake_file_dir
 
 echo "finished with exit code $? at: `date`"
 
